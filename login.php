b440f3b1cf635dd4319e5840d925a8b5c5f98b85
Thennavan-Hex <thensthennavan3@gmail.com>
2023-07-29 19:47:38 +0530

    new

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./assests/style.css">
 </head>
 <body>
     <div class="container d-flex justify-content-center">
